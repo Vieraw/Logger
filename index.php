e8ad219d517f78f1c053313e05f8789f107147a1
Vieraw <vieraw@quissa.pw>
2018-01-30 09:46:55 +0300

    Added exception handling

--- index.php
+++ index.php
@@ -1,35 +1,41 @@
 <?php
 include_once 'vendor/autoload.php';
 
-$loggers = new SplObjectStorage();
+try
+{
+    $loggers = new SplObjectStorage();
 
-$loggers->attach(new Log\Loggers\DatabaseLogger([
-    'db' => (function ()
-    {
-        $pdo = new \PDO('mysql:host=localhost;dbname=db;', 'username', 'password');
-        $pdo->exec('set names utf-8');
-        return $pdo;
-    })(),
-    'table' => 'logs'
-]));
+    $loggers->attach(new Log\Loggers\DatabaseLogger([
+        'db' => (function () {
+            $pdo = new \PDO('mysql:host=localhost;dbname=db;', 'username', 'password');
+            $pdo->exec('set names utf8');
+            return $pdo;
+        })(),
+        'table' => 'logs'
+    ]));
 
-$loggers->attach(new Log\Loggers\FileLogger([
-    'path' => 'default.log'
-]));
+    $loggers->attach(new Log\Loggers\FileLogger([
+        'path' => 'default.log'
+    ]));
 
-$loggers->attach(new Log\Loggers\MailNativeLogger([
-    'to' => 'email@mail.com',
-    'subject' => 'MailLoggerError',
-    'from' => 'from@mail.com',
-    'levels' => ['error']
-]));
-$log = new Log\Logger($loggers);
+    $loggers->attach(new Log\Loggers\MailNativeLogger([
+        'to' => 'email@mail.com',
+        'subject' => 'MailLoggerError',
+        'from' => 'from@mail.com',
+        'levels' => ['error']
+    ]));
+    $log = new Log\Logger($loggers);
 
-$log->info("Info message");
-$log->alert("Alert message");
-$log->error("Error message");
-$log->debug("Debug message");
-$log->notice("Notice message");
-$log->warning("Warning message");
-$log->critical("Critical message");
-$log->emergency("Emergency message");
+    $log->info('Info message');
+    $log->alert('Alert message');
+    $log->error('Error message');
+    $log->debug('Debug message');
+    $log->notice('Notice message');
+    $log->warning('Warning message');
+    $log->critical('Critical message');
+    $log->emergency('Emergency message');
+}
+catch (Throwable $e)
+{
+    echo $e->getMessage();
+}
